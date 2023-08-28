3f48fa88bcf61aa8fb13937cd302d54109b5aa07
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-28 14:09:51 -0400

    Implement the playRound function
    playRound function is the function that plays one round of the game.
    It returns a string that describe who win the game.

--- script.js
+++ script.js
@@ -4,8 +4,52 @@ function getRandomInt(max) {
   
 
 function getComputerChoice(){
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     return choices[getRandomInt(choices.length)];
     
 }
-console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock"){
+        if (computerSelection == "paper"){
+            return "You lose, the paper beats the rock";
+        } else if (computerSelection == "scissors"){
+            return "You win, the scissors beat the rock";
+        } else{
+            return "You both choosed the same thing";
+        }
+    } else if (playerSelection == "paper"){
+        if (computerSelection == "scissors"){
+            return "You lose, the scissors beat the paper";
+        } else if (computerSelection == "rock"){
+            return "You win, the paper beats the rock";
+        } else{
+            return "You both choosed the same thing";
+        }
+    } else if (playerSelection == "scissors"){
+        if (computerSelection == "rock"){
+            return "You lose, the rock beats the scissors";
+        } else if (computerSelection == "paper"){
+            return "You win, the scissors beats the paper";
+        } else{
+            return "You both choosed the same thing";
+        }
+    } else{
+        return "Dear user, your choice is not correct!";
+    }
+
+}
+
+let playerchoice = prompt("Welcome dear player, Please choose between: 'rock', 'paper', 'scissors'");
+
+let computerchoice = getComputerChoice();
+
+console.log(`computer choice => ${computerchoice}
+playerchoice => ${playerchoice}`);
+
+
+
+console.log(playRound(playerchoice, computerchoice));
