082bc70e03e500d4ed4e9733ab72c5b1fd9c6cd1
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-08-28 15:00:37 -0400

    Release version 1.0.0 Project RPS
    RPS means Rock Paper Scissors

--- script.js
+++ script.js
@@ -15,41 +15,61 @@ function playRound(playerSelection, computerSelection){
 
     if (playerSelection == "rock"){
         if (computerSelection == "paper"){
-            return "You lose, the paper beats the rock";
+            return ["You lose, the paper beats the rock", 0];
         } else if (computerSelection == "scissors"){
-            return "You win, the scissors beat the rock";
+            return ["You win, the rock beats the scissors", 1];
         } else{
-            return "You both choosed the same thing";
+            return ["You both choosed the same thing", 2];
         }
     } else if (playerSelection == "paper"){
         if (computerSelection == "scissors"){
-            return "You lose, the scissors beat the paper";
+            return ["You lose, the scissors beat the paper", 0];
         } else if (computerSelection == "rock"){
-            return "You win, the paper beats the rock";
+            return ["You win, the paper beats the rock", 1];
         } else{
-            return "You both choosed the same thing";
+            return ["You both choosed the same thing", 2];
         }
     } else if (playerSelection == "scissors"){
         if (computerSelection == "rock"){
-            return "You lose, the rock beats the scissors";
+            return ["You lose, the rock beats the scissors", 0];
         } else if (computerSelection == "paper"){
-            return "You win, the scissors beats the paper";
+            return ["You win, the scissors beats the paper", 1];
         } else{
-            return "You both choosed the same thing";
+            return ["You both choosed the same thing\nPlease try again...", 2];
         }
     } else{
-        return "Dear user, your choice is not correct!";
+        return ["Dear user, your choice is not correct! \nPlease try again...", 2];
     }
 
 }
 
-let playerchoice = prompt("Welcome dear player, Please choose between: 'rock', 'paper', 'scissors'");
+function game(){
+    let rounds = 1;
+    let score = 0;
+    while (rounds <= 5){
+        let playerchoice = prompt("Welcome dear player, Please choose between: 'rock', 'paper', 'scissors'");
 
-let computerchoice = getComputerChoice();
+        let computerchoice = getComputerChoice();
 
-console.log(`computer choice => ${computerchoice}
-playerchoice => ${playerchoice}`);
+        let result = playRound(playerchoice, computerchoice);
+        if (result[1] == 1){
+            console.log(result[0]);
+            score++;
+        } else if(result[1] == 0){
+            console.log(result[0]);
+        } else if (result[1] == 2){
+            console.log(result[0]);
+            continue;
+        }
+        console.log(`Computer choice => ${computerchoice}
+Playerchoice => ${playerchoice}
+Round: ${rounds}`);
+        rounds++;
+    }
+
+    console.log(`GAME OVER
+Your score is ${score}/5`);
+}
 
 
-
-console.log(playRound(playerchoice, computerchoice));
+game();
